f14e83c6b95dc018f10e4f3a2048e8f4c9aa30cd
jorge <jbousing@gmail.com>
2023-09-11 18:01:32 -0400

    component working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { InputTags } from './TagsInput/InputTags';
 
 function App() {
+  
+  
+  const [tags, setTags] = useState<string[]>([]);
+
+  useEffect(() => {
+    const firstData = ["tag1","tag2","tag3"];
+
+    setTags(firstData);
+
+  },[]);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className='app-container'>
+        <div className='app-input-container'>
+        <InputTags
+          tags={tags} 
+          updateTags={setTags}
+        />
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/TagsInput/InputTags.tsx
+++ src/TagsInput/InputTags.tsx
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import '../syles/style.css';
 
-export const InputTags = () => {
+interface IProps {
+    tags: string[];
+    updateTags: (tags: string[]) => void;
+
+};
+
+export const InputTags = (props: IProps) => {
+
+  const [input, setInput] = useState<string>('');
+  const [tags, setTags] = useState<string[]>([]);
+  const [isKeyReleased, setIsKeyReleased] = useState(false);
+
+  useEffect(() => {
+    setTags(tags);
+  },[]);
+
+  useEffect(() => {
+    console.log("mostrando tags ",tags);
+  },[tags]);
+
+  const onChange = (e: { target: HTMLInputElement; }) => {
+    const { value } = e.target as HTMLInputElement;
+    setInput(value);
+  };
+
+  const onKeyDown = (e: { preventDefault?: any; key?: any; }) => {
+    const { key } = e;
+    const trimmedInput = input.trim();
+  
+    if (key === ',' && trimmedInput.length && !tags.includes(trimmedInput)) {
+      e.preventDefault();
+      setTags(prevState => [...prevState, trimmedInput]);
+      setInput('');
+    }
+  
+    if (key === "Backspace" && !input.length && tags.length && isKeyReleased) {
+      const tagsCopy = [...tags];
+      const poppedTag = tagsCopy.pop();
+      e.preventDefault();
+      setTags(tagsCopy);
+      setInput(poppedTag as string);
+    }
+  
+    setIsKeyReleased(false);
+  };
+  
+  const onKeyUp = () => {
+    setIsKeyReleased(true);
+  }
+
+  const deleteTag = (index: number) => {
+    setTags(prevState => prevState.filter((tag, i) => i !== index))
+  }
+
   return (
-    <div>InputTags</div>
+    <div className="container">  
+        {tags.map((tag: string, index: number) => (
+            <div className="tag" key={tag}>
+                {tag}
+                <button onClick={() => deleteTag(index)}>x</button>
+            </div>
+        ))}
+        <input
+            value={input}
+            placeholder="Enter a tag"
+            onKeyDown={onKeyDown}
+            onKeyUp={onKeyUp}
+            onChange={onChange}
+        />
+</div>
   )
 }
