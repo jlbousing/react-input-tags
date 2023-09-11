b348943d03aa5f35fb920bbf4a29f9ef35ee8ea0
jorge <jbousing@gmail.com>
2023-09-11 18:39:15 -0400

    remove images and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { InputTags } from './TagsInput/InputTags';
 
 function App() {
@@ -17,10 +15,6 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
       <div className='app-container'>
         <div className='app-input-container'>
         <InputTags
